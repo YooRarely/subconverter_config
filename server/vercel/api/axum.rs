8c30b11e20fae0656d1a3f492e6a8c85077f119f
YooRarely <757171400@qq.com>
2026-02-26 03:53:05 +0800

    rust 服务器适配 vercel

---
+++ server/vercel/api/axum.rs
@@ -0,0 +1,11 @@
+use core::router;
+use tower::ServiceBuilder;
+use vercel_runtime::Error;
+use vercel_runtime::axum::{VercelLayer, stream_response};
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let app = ServiceBuilder::new()
+        .layer(VercelLayer::new())
+        .service(router());
+    vercel_runtime::run(app).await
+}
